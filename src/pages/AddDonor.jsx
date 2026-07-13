6aebd48df17503b4d3cd4bd11ccbab45e6b8efb8
Joshua J <jj4440024@gmail.com>
2026-07-13 09:31:23 +0530

    Create Add Donor component

--- src/pages/AddDonor.jsx
+++ src/pages/AddDonor.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const AddDonor = () => {
   return (
     <div className="container mt-5">
-      <h2>Add Donor</h2>
+
+      <div className="card shadow">
+
+        <div className="card-header bg-danger text-white">
+          <h3>Add Blood Donor</h3>
+        </div>
+
+        <div className="card-body">
+
+          <form>
+
+          </form>
+
+        </div>
+
+      </div>
+
     </div>
   );
 };
